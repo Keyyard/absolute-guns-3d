42ab530e59ee40eb0491327fe31ec6a2ae4dc148
AzozGamer936 <azozgamer465@gmail.com>
2026-01-10 20:33:54 +0000

    Changed text of main

--- scripts/main.ts
+++ scripts/main.ts
@@ -120,7 +120,7 @@ class GameController {
     } catch {}
     const messages = [
       `§4Absolute Guns §l3 §rBeta v1.1`,
-      `Welcome Back! this is a beta version, it's §uonly 5%% complete§r from what we envision for the addon. §4Please be patient§f.`,
+      `Welcome Back! this is a beta version, it's §uonly 7%% complete§r from what we envision for the addon. §4Please be patient§f.`,
       `Join our discord to report bugs: §bdiscord.gg/5zeEqwFbww`,
       `And check out our Github: github.com/Keyyard/absolute-guns-3d`,
       `Please forward all bugs and suggestions there in the Issues tab!`,
